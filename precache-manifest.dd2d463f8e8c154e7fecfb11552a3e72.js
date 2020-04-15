a66044f12cf84862abec379f95481e62b5c26c55
Tubagus Fikri Fatoni <fikri.fatoni@doterb.com>
2020-04-15 14:02:35 +0700

    Updates

---
+++ precache-manifest.dd2d463f8e8c154e7fecfb11552a3e72.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7f41a7674538c44c38772978aba3502f",
+    "url": "/my-first-react/index.html"
+  },
+  {
+    "revision": "51623cc390b7a52476d8",
+    "url": "/my-first-react/static/css/main.fc29d31f.chunk.css"
+  },
+  {
+    "revision": "a02a18377568bb6d0465",
+    "url": "/my-first-react/static/js/2.e10ed757.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/my-first-react/static/js/2.e10ed757.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "51623cc390b7a52476d8",
+    "url": "/my-first-react/static/js/main.50fc0d8d.chunk.js"
+  },
+  {
+    "revision": "4f32543a4f6bfd5cfff6",
+    "url": "/my-first-react/static/js/runtime-main.1db36e6e.js"
+  }
+]);
